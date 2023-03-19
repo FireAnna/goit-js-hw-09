c042b9feb5517dba54ace987f18ca3733e42d3a1
Anna Kucherenko <kucherenko.place@icloud.com>
2023-03-19 14:14:01 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const form = document.querySelector('form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const { delay, step, amount } = e.target;
+  for (let i = 0; i < amount.value; i++) {
+    let del = +delay.value + step.value * i;
+    createPromise(i + 1, del)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promiseTime = new Promise((resolve, rejected) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      }
+      // Reject
+      rejected({ position, delay });      
+    }, delay);
+  });
+  return promiseTime;
 }
